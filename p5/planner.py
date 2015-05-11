35bf8f80b0e40f982fc62068c856ac231748d55f
Jarret Belles <jbelles@ucsc.edu>
2015-05-10 18:05:33 -0700

    better

--- p5/planner.py
+++ p5/planner.py
@@ -36,7 +36,6 @@ def make_initial_state(inventory):
     return initial_state
 
 def make_checker(rule):
-    # this code runs once
     requires = None
     consumes = None
     if 'Requires' in rule:
@@ -45,7 +44,6 @@ def make_checker(rule):
         consumes = rule['Consumes']
 
     def check(state):
-        # this code runs millions of times
         if consumes:
             for i in consumes:
                 if state[items[i]] < consumes[i]:
@@ -54,22 +52,17 @@ def make_checker(rule):
             for i in requires:
                 if state[items[i]] < 1:
                     return False
-        # if the above conditions are satisfied, return true.
         return True
 
     return check
 
 def make_effector(rule):
-    # sets up the effector that will effect the state when called.
     produces = rule['Produces']
     consumes = None
-    # check to see if the recipe consumes anything
     if 'Consumes' in rule:
         consumes = rule['Consumes']
 
     def effect(state):
-        # this code runs millions of times
-        # copy current state
         next_state = list(state)
         if consumes:
             for i in consumes:
@@ -97,9 +90,7 @@ def search(recipes, initial_state, goal, is_goal, limit, heuristic):
 
     while not is_goal(curr_state, goal):
         for recipe in recipes:
-            # For each possible recipe from our current state:
             if recipe.check(curr_state):
-                # Use the recipe, then update distance and previous nodes.
                 new_state = tuple(recipe.effect(curr_state))
                 if new_state not in disc:
                     prev[new_state] = tuple(curr_state)
@@ -109,10 +100,8 @@ def search(recipes, initial_state, goal, is_goal, limit, heuristic):
                     recipe_name[tuple(curr_state)] = recipe.name
 
                 elif dist[new_state] > dist[curr_state] + recipe.cost:
-                    # replace value if we have found a 'faster' route
                     prev[new_state] = tuple(curr_state)
                     dist[new_state] = dist[curr_state] + recipe.cost
-                    # recipe_name[tuple(curr_state)] = recipe.name
         curr_state = heappop(queue)[1]
     print "new state", curr_state
 
@@ -122,23 +111,15 @@ def search(recipes, initial_state, goal, is_goal, limit, heuristic):
     recipe_name[curr_state] = "Goal"
 
     while curr_state != None:
-        # append recipe names
         path.append(recipe_name[curr_state])
-        # append states
-        #path.append(curr_state)
         print curr_state
         curr_state = prev[curr_state]
-    #print path
     path.append("Start")
-    #print items
 
     for i in range(len(path)):
         print[path[len(path)-i-1]]
 
-
-    #return an array of points
     return total_cost, path
-# End A*
 
 
 def main():
@@ -153,14 +134,8 @@ def main():
 
 
     print search(all_recipes, state, goal, goal_check, 20, None)
-
-    # print items
     for i in range(100):
         for recipe in all_recipes:
             if recipe.check(state):
                 state = recipe.effect(state)
-        # print state
-    # print items
-
-# End def main
 main()
